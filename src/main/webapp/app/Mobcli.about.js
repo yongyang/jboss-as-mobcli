9bed6d67bffc01f1d22ac6ca1eee6885bb5474c6
yyang <yyang@redhat.com>
2013-06-03 16:35:52 +0800

    About

--- src/main/webapp/app/Mobcli.about.js
+++ src/main/webapp/app/Mobcli.about.js
@@ -4,7 +4,18 @@ Ext.define('Mobcli.about.AboutPanel', {
         title: 'About',
         iconCls: 'info',
         cls: 'card',
-        html: 'Mobile CLI for JBoss AS7'
+        items: [
+            {
+                xtype: 'toolbar',
+                docked: 'top',
+                title: 'About'
+            },
+            {
+                xtype: 'label',
+                styleHtmlContent: 'true',
+                html: '<div style="text-align: center; vertical-align: middle"><h2>Mobile CLI for JBoss AS7</h2> <b>Contact:</b> yyang@redhat.com</div>'
+            }
+        ]
     }
     
 });
